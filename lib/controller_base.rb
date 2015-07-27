d7febbf64ab86b4d0aaef9c88a69638e561693a4
reichd <dancreich@gmail.com>
2015-07-26 23:33:11 -0400

    Render response content from template.

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -2,8 +2,28 @@ class ControllerBase
   attr_reader :req, :res
 
   def initialize(req, res)
-  @req = req
-  @res = res
+    @req = req
+    @res = res
   end
-  
+
+  def already_built_response?
+    @already_build_response
+  end
+
+  def render_content(content, content_type)
+    raise if already_built_response?
+
+    @res.content_type = content_type
+    @res.body = content
+
+    @already_built_response = true
+  end
+
+  def render(template_name)
+    template_file = "views/#{ActiveSupport::Inflector.underscore(self.class.name)}/#{template_name}.html.erb"
+    template = File.read(template_file)
+    content = ERB.new(template)
+
+    render_content(content, "text/html")
+  end
 end
