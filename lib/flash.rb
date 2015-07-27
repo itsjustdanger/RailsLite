58bb449fd4efe675b3aeccb6420f7a429691cfd7
reichd <dancreich@gmail.com>
2015-07-27 00:23:58 -0400

    Allow developer to use flashes.

---
+++ lib/flash.rb
@@ -0,0 +1,23 @@
+class Flash
+
+  attr_accessor :flashes, :now
+
+  def initialize(flashes = {})
+    @flashes = flashes.stringify_keys
+    @now = nil
+  end
+  
+  def [](key)
+    @flashes[key.to_s]
+  end
+
+  def []=(key, value)
+    @flashes[key.to_s] = value
+  end
+
+  def now()
+    @now ||= Flash.new(self)
+    return @now.flashes.values
+    @now = nil
+  end
+end
