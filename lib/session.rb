4aa8062f9f4a2391d6f9b679a2249ac977eea948
reichd <dancreich@gmail.com>
2015-07-26 23:42:10 -0400

    Patch bracket methods and store session from response.

--- lib/session.rb
+++ lib/session.rb
@@ -11,4 +11,16 @@ class Session
       @cookie = {}
     end
   end
+
+  def [](key)
+    @cookie[key]
+  end
+
+  def []=(key, val)
+    @cookie[key] = val
+  end
+
+  def store_session(res)
+    res.cookies << WEBrick::Cookie.new("_rails_lite_app", @cookie.to_json)
+  end
 end
