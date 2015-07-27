3b38c78a3f4721c7b74834d4d56cf54cfc5039ed
reichd <dancreich@gmail.com>
2015-07-27 00:08:48 -0400

    Add router and store basic request inputs.

---
+++ lib/router.rb
@@ -0,0 +1,14 @@
+class Router
+
+  attr_reader
+
+  def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
+  end
+
+  
+
+end
